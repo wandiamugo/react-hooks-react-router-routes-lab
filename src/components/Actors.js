48afb8aa7dc27f8aae240c371bf3401ab82b1893
Wandia E Mugo <wwandia173@gmail.com>
2022-07-20 16:45:11 +0300

    commit tree d9c46779dc384f6674bbd907f8c34dc4fdb1c67c

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorsList= actors.map((actor) => (
+    <div key={actor.name}>
+      <h1>{actor.name}</h1>
+      <ul>
+        <li>
+          {actor.movies}
+        </li>
+      </ul>
+    </div>
+  ))
+  return <div>
+    <h1>Actors Page</h1>
+    {actorsList}
+  </div>;
 }
 
 export default Actors;
