8f721ce0758000fff236e8c11ef1b596a3ecd394
Wandia E Mugo <wwandia173@gmail.com>
2022-07-20 16:45:19 +0300

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorsList= directors.map((director) => (
+  <div key={director.name}>
+    <h2>{director.name}</h2>
+    <ul>
+      <li>{director.movies}</li>
+    </ul>
+  </div>
+  
+  
+  ))
+  return <div>
+    <h1>Directors Page</h1>
+    {directorsList}
+  </div>;
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,19 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const moviesList= movies.map((movie) => (
+    <div key={movie.title}>
+      <h1>{movie.title}</h1>
+      <p>{movie.time}</p>
+      <ul>
+        <li>{movie.genres}</li>
+      </ul>
+    </div>
+  ))
+  return <div>
+    <h1>Movies Page</h1>
+    {moviesList}
+  </div>;
 }
 
 export default Movies;
